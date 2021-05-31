1f49f815769f2f20b51a8fb029c0504556463727
Luke-pak <icbm07@hanmail.net>
2021-05-31 12:29:11 +0900

    002- Modify AgGrid

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
+import { useState, useEffect, useRef } from 'react';
+import { AgGridReact, AgGridColumn } from 'ag-grid-react';
 
-import './App.css';
-
-import {AgGridReact} from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-balham.css';
-
-import {useRecoilValue} from 'recoil';
-
-import {AgState} from './Atoms';
+// import 'ag-grid-community/dist/styles/ag-grid.css';
+// import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import 'ag-grid-enterprise';
+//scss를 사용할려면 npm install sass를 해준다.
+import './App.scss';
 
 function App() {
-  const state = useRecoilValue(AgState);
+  //const [state, setState] = useRecoilState(AgState);
+  const [rowData, setRowData] = useState([]);
+  const [gridApi, setGridApi] = useState();
+  const gridRef = useRef(null);
 
-  console.log(state);
+  useEffect(() => {
+    fetch('https://www.ag-grid.com/example-assets/row-data.json')
+      .then(result => result.json())
+      .then(rowData => setRowData(rowData));
+  }, []);
+
+  const onButtonClick = () => {
+    const selectedNodes = gridApi.getSelectedNodes();
+    const selectedData = selectedNodes.map(node => node.data);
+    const selectedDataStringPresentation = selectedData
+      .map(node => node.make + ' ' + node.model)
+      .join(', ');
+    alert(`Selected Nodes : ${selectedDataStringPresentation}`);
+  };
+
+  const gridOptions = {
+    // PROPERTIES
+    // Objects like myRowData and myColDefs would be created in your application
+    // rowData: rowData,
+    // ref: gridRef,
+    pagination: true,
+    rowSelection: 'single',
+
+    // EVENTS
+    // Add event handlers
+    onRowClicked: event => console.log('A row was clicked'),
+    onColumnResized: event => console.log('A column was resized'),
+    onGridReady: params => setGridApi(params.api),
+
+    // CALLBACKS
+    isScrollLag: () => false,
+  };
 
   return (
-    <div className='ag-theme-balham' style={{width: 600, height: 800}}>
-      <AgGridReact columnDefs={state.columnDefs} rowData={state.rowDatas}/>
+    <div className="ag-theme-alpine" style={{ width: 800, height: 800 }}>
+      <button onClick={onButtonClick}>Get Selected Rows</button>
+      <AgGridReact
+        ref={gridRef}
+        rowData={rowData}
+        // rowSelection="multiple"
+        // onGridReady={params => setGridApi(params.api)}
+        gridOptions={gridOptions}
+      >
+        <AgGridColumn
+          field="make"
+          sortable={true}
+          filter={true}
+          checkboxSelection={true}
+          // rowGroup={true}
+        ></AgGridColumn>
+        <AgGridColumn
+          field="model"
+          sortable={true}
+          filter={true}
+        ></AgGridColumn>
+        <AgGridColumn
+          field="price"
+          sortable={true}
+          filter={true}
+        ></AgGridColumn>
+      </AgGridReact>
     </div>
   );
 }
--- src/Atoms.js
+++ src/Atoms.js
@@ -1,17 +1,24 @@
-import {atom} from 'recoil';
+import { atom } from 'recoil';
 
 export const AgState = atom({
-    key : 'agAtom',
-    default:{
-        columnDefs:[
-            {headerName: 'Make', field:'make'},
-            {headerName: 'Model', field:'model'},
-            {headerName: 'Price', field:'price'},
-        ],
-        rowDatas:[
-            {make: 'Toyota', model:'Celica', price: 30000}
-            ,{make: 'Ford', model:'Mondeo', price: 31000}
-            ,{make: 'Porsche', model:'Boxter', price: 35000}
-        ]
-    }
-})
+  key: 'agAtom',
+  default: {
+    columnDefs: [
+      {
+        headerName: 'Make',
+        field: 'make',
+        sortable: true,
+        filter: true,
+        rowGroup: true,
+      },
+      { headerName: 'Model', field: 'model', sortable: true, filter: true },
+      { headerName: 'Price', field: 'price', sortable: true, filter: true },
+    ],
+    rowData: null,
+    // rowData: [
+    //   { make: 'Toyota', model: 'Celica', price: 30000 },
+    //   { make: 'Ford', model: 'Mondeo', price: 31000 },
+    //   { make: 'Porsche', model: 'Boxter', price: 35000 },
+    // ],
+  },
+});
